84d63608347f47e2b95cc5b7fb48a099622fbaba
Edigeev Galymka <galymtashtek@gmail.com>
2025-08-10 12:07:46 +0600

    Add suppressHydrationWarning prop to body element in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,7 +50,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="antialiased">
+      <body className="antialiased" suppressHydrationWarning={true}>
         <a 
           href="#main-content"
           className="sr-only focus:not-sr-only focus:absolute focus:top-4 focus:left-4 focus:z-50 focus:px-4 focus:py-2 focus:bg-black focus:text-white focus:rounded focus:outline-none focus:ring-2 focus:ring-white"
